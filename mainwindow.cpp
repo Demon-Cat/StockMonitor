9fee59ad73e19ec954c0940b583b4eb0d3ad9b32
Demon-Cat <870603664@qq.com>
2019-03-13 22:15:40 +0800

    完成界面移动

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,49 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMouseEvent>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowFlags(Qt::Tool | Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint);
+    setAttribute(Qt::WA_TranslucentBackground);
+
+    m_manager = new QNetworkAccessManager(this);
+
+    m_timer = new QTimer(this);
+    connect(m_timer, &QTimer::timeout, this, &MainWindow::onTimer);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    m_isPressed = true;
+    m_pressedPos = event->globalPos() - pos();
+    QMainWindow::mousePressEvent(event);
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *event)
+{
+    m_isPressed = false;
+    QMainWindow::mouseReleaseEvent(event);
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    if (m_isPressed)
+    {
+        move(event->globalPos() - m_pressedPos);
+    }
+    QMainWindow::mouseMoveEvent(event);
+}
+
+void MainWindow::onTimer()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +19,22 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+
+private slots:
+    void onTimer();
+
 private:
     Ui::MainWindow *ui;
+
+    bool m_isPressed = false;
+    QPoint m_pressedPos;
+
+    QNetworkAccessManager *m_manager;
+    QTimer *m_timer;
 };
 
 #endif // MAINWINDOW_H
